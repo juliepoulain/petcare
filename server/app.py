8854517e86ff06b3068e93f01648dd7cdff7437c
Julie Poulain <juliempoulain@gmail.com>
2024-05-31 13:35:53 -0700

    add build folder

--- server/app.py
+++ server/app.py
@@ -7,20 +7,8 @@ from models import Owner, Pet, Sitter, Visit, db
 from app import api
 from flask_cors import CORS
 
-
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 DATABASE = os.environ.get("DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
-
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.json.compact = False
-
-migrate = Migrate(app, db)
-
-db.init_app(app)
-CORS(app)
-api = Api(app)
 
 @app.route("/api")
 def index():
